2d410db3bcb34485a5dafd8e21ca3d0c0b763de2
Brice601 <brice.dlp@gmail.com>
2024-08-29 10:23:04 +0200

    creation projets dynamiquement modale

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -3,6 +3,7 @@ const editionMode = document.querySelector("#modifier-projet div");
 console.log(editionMode);
 
 let affichageModale = null;
+const crossModal = document.querySelector(".modal-close");
 
 /** function Affichage Modale */
 const openModal = function (e) {
@@ -13,6 +14,10 @@ const openModal = function (e) {
     /** fermeture boite modale hors croix */
     affichageModale.addEventListener("click", closeModal);
     affichageModale.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
+    /** fermeture modale clic croix */
+    
+    // console.log("croix cliquée");
+    crossModal.addEventListener("click", closeModal);
 }
 
 /** function Fermeture boite modale */
@@ -37,7 +42,34 @@ const stopPropagation = function (e) {
 /** affichage modale clic mode edition */
 editionMode.addEventListener("click", openModal);
 
-/** fermeture modale clic croix */
-const crossModal = document.querySelector(".modal-close");
-// console.log("croix cliquée");
-crossModal.addEventListener("click", closeModal);
+
+/** Récupération des projets */
+const reponseProjets = await fetch("http://localhost:5678/api/works");
+const projets = await reponseProjets.json();
+console.log(projets);
+const modalGallery = document.querySelector(".modal-gallery");
+// console.log(modalGallery);
+/** Suppression projets */
+modalGallery.innerHTML = "";
+
+/** Ajout de ts les projets */
+for (let i = 0; i < projets.length; i++ ) {
+    /** création du projet */
+    const projet = projets[i];
+    const figure = document.createElement("figure")
+    const imageProjet = document.createElement("img");
+    const trashProjet = document.createElement("img");
+    /** ajout des attibuts, classes */
+    imageProjet.src = projet.imageUrl;
+    imageProjet.alt = projet.titre;
+    trashProjet.src = "./assets/icons/trash-can-solid.svg";
+    trashProjet.alt = "poubelle"
+    imageProjet.classList.add("modal-projet");
+    trashProjet.classList.add("trash");
+    figure.classList.add("modal-projet");
+    /** composition du projet */
+    figure.appendChild(imageProjet);
+    figure.appendChild(trashProjet);
+    /** ajout au dom */
+    modalGallery.appendChild(figure);
+}
